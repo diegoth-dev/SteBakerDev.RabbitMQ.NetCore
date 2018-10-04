7908307825d4f218336e44d824fab11a6c4dacf1
Steve Baker <ste.baker.92@gmail.com>
2018-10-04 13:50:58 +0100

    Added Microsoft eShop implemetation for RabbitMQ

---
+++ EventBus/Abstractions/IIntegrationEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using SteBakerDev.EventBus.Events;
+using System.Threading.Tasks;
+
+namespace SteBakerDev.EventBus.Abstractions
+{
+    public interface IIntegrationEventHandler<in TIntegrationEvent> : IIntegrationEventHandler 
+        where TIntegrationEvent: IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+
+    public interface IIntegrationEventHandler
+    {
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SteBakerDev.RabbitMQ.NetCore
-A NuGet package using the eShop on containers implementation of RabbitMQ
+A NuGet package using the eShop on containers implementation of RabbitMQ with some added helper methods
+
+[Source](https://github.com/dotnet-architecture/eShopOnContainers/)
+
+
+## Usage
+
